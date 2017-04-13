91bfd7b11209eb14a59b3cb221521f39526778be
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-04-13 14:14:23 +0200

    Setup ios project

---
+++ HelloHereSwift/HelloHereSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloHereSwift
+//
+//  Created by Chakkra CHAK on 12/04/2017.
+//  Copyright © 2017 Chakkra CHAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
